99575cfd63d4428532db6fea3e805c95fc595bf6
Paul Solt <PaulSolt@gmail.com>
2020-05-22 13:25:01 -0400

    Added starter project with slicing, and assets

---
+++ iOS15-SketchDesign-Health-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS15-SketchDesign-Health-App
+//
+//  Created by Paul Solt on 5/22/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
